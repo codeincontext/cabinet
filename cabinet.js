7933d94c9d1171a8c51c5d5c1a01f00d21af9b39
Adam Howard <adz@skatty.me>
2014-03-08 16:45:07 +0000

    Initial Commit

---
+++ cabinet.js
@@ -0,0 +1,73 @@
+Cabinet = {};
+Players = new Meteor.Collection('players');
+Tasks = new Meteor.Collection('tasks');
+Situations = new Meteor.Collection('situations');
+
+
+if (Meteor.isClient) {
+  Template.task.events({
+    'click': function () {
+      // template data, if any, is available in 'this'
+      console.log(this);
+      console.log("You pressed the button");
+      // Situations.remove(this._id);
+    }
+  });
+
+  Meteor.startup(function () {
+
+  });
+
+  Meteor.subscribe('tasks', function () {
+
+    for (var i = 0; i < 4; i++) {
+      var taskCount = Tasks.find().count();
+      var randomID = Math.floor((Math.random()*taskCount));
+      var task = Tasks.findOne({}, {skip: randomID});
+
+      var button = Meteor.render(Template.task(task));
+      $('body').append(button);
+    }
+
+    // TODO: make this more reactive
+    $('.task-button').on('click', function() {
+      var taskID = $(this).data('id');
+      console.log('resolved task: ' + taskID);
+
+      var situationsToResolve = Situations.find({taskID: taskID}).fetch();
+      if (situationsToResolve.length) {
+        situationsToResolve.forEach(function(situation) {
+          Situations.remove(situation._id);
+        });
+        console.log('yay!');
+      } else {
+        console.log('boo!');
+      }
+    });
+
+    // console.log(task);
+    // console.log(button);
+  });
+
+}
+
+if (Meteor.isServer) {
+  if (Tasks.find().count() == 0) {
+    console.log("Tasks list is empty. Populating from file");
+    _.each(Assets.getText("tasks.txt").split("\n"), function (line) {
+      Tasks.insert({name: line});
+    });
+  }
+
+  Meteor.publish("tasks", function () {
+    return Tasks.find({});
+  });
+
+  Meteor.onConnection(function () {
+    console.log("User connected. Should assign cards");
+  });
+
+  Meteor.startup(function () {
+    // code to run on server at startup
+  });
+}
